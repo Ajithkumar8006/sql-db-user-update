1380ddc9fb737b383da2fdb541d93ab79e768490
Ajithkumar8006 <83852404+Ajithkumar8006@users.noreply.github.com>
2025-07-31 23:21:32 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+def read_secret(path):
+    try:
+        with open(path) as f:
+            return f.read().strip()
+    except Exception as e:
+        raise RuntimeError(f"❌ Failed to read secret from {path}: {e}")
+
+DB_CONFIG = {
+    'host': '10.15.69.5',
+    'port': 5432,
+    'dbname': 'testdb',
+    'user': 'pgadmin',
+    'password': read_secret('/secrets/admin-password/pg-admin-user-password'),
+    'sslmode': 'verify-ca',
+    'sslrootcert': '/secrets/server-ca/server-ca.pem',
+    'sslcert': '/secrets/client-cert/client-cert.pem',
+    # 'sslkey': '/secrets/client-key/client-key.pem'
+}
+
+TARGET_USER = 'appuser'
+PRIVILEGES = ['UPDATE']
